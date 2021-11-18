2c0faa70ce42f723504d44f17d54dee66c58b492
kolotov <25168302+kolotov@users.noreply.github.com>
2021-11-18 22:02:35 +0200

    add database

---
+++ config/init.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Nov 18, 2021 at 02:16 PM
+-- Server version: 10.6.4-MariaDB-1:10.6.4+maria~focal
+-- PHP Version: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS `database`;
+USE `database`;
+
+--
+-- Database: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `balance`
+--
+
+CREATE TABLE `balance` (
+  `balance_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `balance_curency` char(3) NOT NULL,
+  `balance_value` decimal(10,2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `balance`
+--
+
+INSERT INTO `balance` (`balance_id`, `user_id`, `balance_curency`, `balance_value`) VALUES
+(1, 1, 'usd', '200.00'),
+(3, 1, 'eur', '100.00'),
+(4, 1, 'rub', '5000.00'),
+(5, 2, 'usd', '1000.00'),
+(6, 2, 'eur', '200.00'),
+(7, 2, 'rub', '11000.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `contact_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `contact_type` char(5) NOT NULL,
+  `contact_value` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`contact_id`, `user_id`, `contact_type`, `contact_value`) VALUES
+(2, 1, 'email', 'nrostov@mail.com'),
+(3, 1, 'mail', 'nik.rostov@hotmail.com'),
+(4, 2, 'email', 'bagration@mail.com'),
+(5, 1, 'tell', '828382878423'),
+(6, 2, 'tell', '9328492384');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_login` varchar(40) NOT NULL,
+  `user_pswd_hash` varchar(255) NOT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `user_sex` char(1) DEFAULT NULL,
+  `user_born` date DEFAULT NULL,
+  `user_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_login`, `user_pswd_hash`, `user_name`, `user_sex`, `user_born`, `user_status`) VALUES
+(1, 'nrostov', '64687450b702fa37f85855f56e562c0c', 'Nikolai Rostov', 'm', '1869-12-02', 'verified'),
+(2, 'bagration', 'f3c19a399402133d1cbd4f32dc9ba52e', 'Pyotr Bagration', 'm', '1765-07-10', 'verified');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `balance`
+--
+ALTER TABLE `balance`
+  ADD PRIMARY KEY (`balance_id`),
+  ADD KEY `user_id` (`user_id`) USING BTREE;
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`contact_id`),
+  ADD KEY `user_id` (`user_id`) USING BTREE;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_login` (`user_login`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `balance`
+--
+ALTER TABLE `balance`
+  MODIFY `balance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `balance`
+--
+ALTER TABLE `balance`
+  ADD CONSTRAINT `balance` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD CONSTRAINT `contacts` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
