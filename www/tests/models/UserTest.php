bff1a277fee03db575b5708ab0e91deebb29581e
kolotov <25168302+kolotov@users.noreply.github.com>
2021-11-19 04:36:56 +0200

    feat: add tests for user model

---
+++ www/tests/models/UserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    private $_user;
+
+    public function setUp(): void
+    {
+        $this->_user = new User();
+        $login = 'bagration';
+        $this->_user->setUser($login);
+    }
+
+    /**
+     * @dataProvider balanceProvider
+     * */
+    public function testBalance(string $currency, float $balance, float $add_money): void
+    {
+        $this->_user->setBalance($currency, $balance);
+        $this->_user->addBalance($currency, $add_money);
+
+        $expected_money = $balance + $add_money;
+        $this->assertEquals($expected_money, $this->_user->getBalance($currency));
+    }
+
+    public function balanceProvider(): array
+    {
+        return [
+            'set 1000.90 usd and add 100.10 '  => ['usd', 1000.90, 100.10],
+            'set 555.11 eur and add -0.90' => ['eur', 555.11, -0.90],
+            'set 300 rub and add 200' => ['rub', 300, 200],
+        ];
+    }
+}
