69ec73a4695362f19d43a0fc52c274f92925b0e8
kolotov <25168302+kolotov@users.noreply.github.com>
2021-11-21 05:16:25 +0200

    feat: add RestApi

---
+++ www/src/Controllers/RestApiController.php
@@ -0,0 +1,182 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sports\Betting\Controllers;
+
+use Sports\Betting\Models\User;
+use Sports\Betting\Views\RestApiView;
+use Exception;
+use ReflectionMethod;
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
+class RestApiController extends AbstractController
+{
+    private AbstractController $_context;
+    private RestApiView $_view;
+    private $_msg;
+    private $_data = [];
+
+    public function __construct()
+    {
+        $this->_view = $this->loadView('RestApiView');
+    }
+
+    public function process(string $entrypoint): void
+    {
+        $rest = $this->_getRestParams(($_SERVER['REQUEST_URI'] ?? ''), $entrypoint);
+
+
+        $method = $_SERVER['REQUEST_METHOD'] ?? '';
+
+        $context = key($rest);
+        $action = $rest[$context]['action'];
+        $path_args = $rest[$context]['args'];
+
+        //supported methods
+
+        // get the raw POST data
+        // header('Content-Type: application/json');
+        //$rawData = file_get_contents("php://input");
+
+        // this returns null if not valid json
+        // print_r($rawData);
+
+        //  print_r($_REQUEST);
+        // exit();
+
+        switch ($method) {
+        case 'GET':
+            $call_func = "get{$context}{$action}";
+            break;
+
+        case 'PUT':
+            $call_func = "put{$context}{$action}";
+            //{"bet":{"currency":"usd","sum":100,"ratio":1.1,"result":0},"winner":1}
+            $json = $_REQUEST['data'] ?? '';
+            $data = json_decode($json, true);
+
+            if (!is_array($data)) {
+                $this->_error("No data received", 400);
+                return;
+            }
+
+            $bet = $data['bet'] ?? [];
+            $winner = $data['winner'] ?? 0;
+
+            $put_data = [$bet, $winner];
+
+            break;
+
+        default:
+            $this->_error("Method not supported", 405);
+            return;
+            break;
+        }
+
+
+        try {
+            $this->_context = $this->getContext($context);
+
+            if (method_exists($this->_context, $call_func) === false) {
+                $this->_error("Action not supported", 404);
+                return;
+            }
+
+
+            //union args for function
+            $args = [...$path_args, ...($put_data ?? [])];
+
+            //check the number of accepted arguments of the function
+            $r = new ReflectionMethod($this->_context, $call_func);
+            if (count($args) !== $r->getNumberOfRequiredParameters()) {
+                $this->_error("Invalid number of arguments", 400);
+                return;
+            }
+
+            //call request method
+            $this->_data = call_user_func_array(array($this->_context, $call_func), $args);
+        } catch (Exception $e) {
+            $this->_error($e->getMessage(), $e->getCode());
+            return;
+        }
+
+        $this->_success("Successfully completed");
+    }
+
+    private function _getRestParams(string $uri, $entrypoint): array
+    {
+        //Example /api/users/{login}/action/balance/{$currency}
+        $path = parse_url($uri, PHP_URL_PATH);
+
+        //remove all symbol except a-z 0-9
+        $sanitized_path = preg_replace('/[^a-z0-9\/]/s', '', mb_strtolower($path));
+
+        $isEntryPoint = strpos($path, $entrypoint) === 0 ? true : false;
+
+        if (!$isEntryPoint) {
+            $this->_error("It isn't entrypoint", 400);
+            return [];
+        }
+
+        [,$endpoint] = explode($entrypoint, $path, 2);
+
+        if (empty($endpoint)) {
+            $this->_error("Request is empty", 400);
+            return [];
+        }
+
+
+        $segments = explode('/action/', $endpoint);
+
+        if (count($segments) < 2) {
+            $this->_error("Bad request", 400);
+            return [];
+        }
+
+        //pasrse segments
+        [$context_segment, $action_segment] = $segments;
+
+        $context = explode('/', $context_segment);
+        $context_name = $context[0];
+        $context_args = array_filter(array_slice($context, 1, 1));
+
+        $action = explode('/', $action_segment);
+        $action_name = $action[0];
+        $action_args = array_filter(array_slice($action, 1));
+
+        //bulding result
+        $result[$context_name]['action'] = $action_name;
+        $result[$context_name]['args'] = [...$context_args, ...$action_args];
+
+        return $result;
+    }
+
+
+
+
+    private function _success($message, $code = 200): void
+    {
+        $this->_msg = ['success' => ['code' => $code, 'message' => $message]];
+        $this->render();
+    }
+
+
+    private function _error($message, $code = null): void
+    {
+        $this->_msg = ['error' => ['code' => $code, 'message' => $message]];
+
+        $this->render();
+    }
+
+    private function render(): void
+    {
+        $data = (empty($this->_data['result'])) ? $this->_msg : [$this->_data['result'], $this->_msg];
+        $this->_view->setOutput($data, ($this->_data['location'] ?? ''));
+        $this->_view->render();
+    }
+}
---
+++ www/src/Views/RestApiView.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sports\Betting\Views;
+
+use Exception;
+
+class RestApiView extends AbstractView
+{
+    private $_json;
+    private $_code;
+    private $_location;
+
+    public function __construct()
+    {
+    }
+
+    public function setOutput(array $data, $location = ''): void
+    {
+        $this->_code = $data['success']['code'] ?? ($data['error']['code'] ?? null);
+        $this->_json = json_encode($data);
+        $this->_location = $location;
+    }
+
+    private function _getHeaderTextByCode(int $code): string
+    {
+        $codes = [
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            204 => 'No Content',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            307 => 'Temporary Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            412 => 'Precondition Failed',
+            415 => 'Unsupported Media Type',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented'
+        ];
+
+        if (isset($codes[$code]) === false) {
+            //throw new Exception('Response code not supported');
+            return '';
+        }
+
+        return $codes[$code];
+    }
+
+    private function _sendHeaders(): void
+    {
+        header('access-Control-Allow-Origin: *');
+        header("access-Control-Allow-Methods: GET, PUT");
+        header("Access-Control-Max-Age: 3600");
+        header("Content-Type: application/json; charset=UTF-8");
+
+        if (!empty($this->_location)) {
+//            header("Location: {$this->_location}");
+        }
+
+        $code = $this->_code;
+
+        //response header
+        if (is_numeric($this->_code)) {
+            $protocol = $_SERVER['SERVER_PROTOCOL'] ?? 'HTTP/1.0';
+
+            $text = $this->_getHeaderTextByCode($this->_code);
+            if (empty($text)) {
+                $code = 500;
+                $text = $this->_getHeaderTextByCode($code);
+            }
+
+            header("{$protocol} {$code} {$text}");
+        }
+    }
+
+    public function render(): void
+    {
+        $this->_sendHeaders();
+        print($this->_json);
+    }
+}
